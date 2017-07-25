3847f77ba1591ad7cc13ae79f5bad58e9232baf5
Venkat Ramachandran <rvenkat@gmail.com>
2017-07-25 20:33:17 +1000

    Added TODOs

--- api.js
+++ api.js
@@ -7,6 +7,8 @@ var getMoviesCb = function(callback) {
         wrapper.getFilmWorldMovies(),
         function(cineworld_movies, filmworld_movies){
             var movies = [];
+            //TODO: MERGE BOTH LISTS
+            //TODO: HANDLE ONE OR BOTH LISTS BEING EMPTY
             movies = movies.concat(cineworld_movies);
             movies = movies.concat(filmworld_movies);
             callback(null, movies);
@@ -18,12 +20,14 @@ var getMovieCb = function(movie_id, callback) {
         wrapper.getCineWorldMovie(movie_id),
         wrapper.getFilmWorldMovie(movie_id),
         function(cineworld_movie, filmworld_movie){
+            //TODO: MERGE BOTH MOVIES
+            //TODO: HANDLE ONE OR BOTH MOVIES BEING EMPTY
             var movie = {};
             movie = JSON.parse(JSON.stringify(cineworld_movie));
             movie.cineworld_id = cineworld_movie && cineworld_movie.id ? movie.id: '';
             movie.filmworld_id = filmworld_movie && filmworld_movie.id ? filmworld_movie.id : '';
             movie.cineworld_price = cineworld_movie && cineworld_movie.Price ? movie.Price: '';
-            movie.filmworld_price = filmworld_movie && filmworld_movie.Price ? filmworld_movie.Price : '';            
+            movie.filmworld_price = filmworld_movie && filmworld_movie.Price ? filmworld_movie.Price : '';
             delete movie.id;
             callback(null, movie);
         });
